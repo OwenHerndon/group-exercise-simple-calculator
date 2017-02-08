cee7c6923e75cd3bbde4d2972fb7577e043b693b
OwenHerndon <owen.w.herndon@gmail.com>
2017-02-08 15:43:33 -0800

    namespace for .test conflict

---
+++ SimpleCalculator.Tests/ConstantsTests/ConstantsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator.Constants;
+
+namespace SimpleCalculatorTests.ConstantsTests
+{
+    [TestClass]
+    public class ConstantsTest
+    {
+        [TestMethod]
+        public void ConstantsInstance()
+        {
+            Constants constant = new Constants();
+            Assert.IsNotNull(constant);
+        }
+    }
+}
